c6b42d1d611d5cfe53405a3ad6355c70130472b4
Andrii <viilka284@gmail.com>
2024-02-12 17:34:37 +0200

    Add esp8266 sketch

---
+++ app/src/main/res/esp_8266/sketch_esp8266_wifi_spot/sketch_esp8266_wifi_spot.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+// APSSID heal, radiation, anomaly
+#ifndef APSSID
+#define APSSID "heal"
+#define APPSK "thereisnospoon"
+#endif
+
+const char *ssid = APSSID;
+const char *password = APPSK;
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
